5939aad8154bd95626304bcd6c6a2bc4c9143007
Navin <navinipe@gmail.com>
2021-03-07 18:22:51 +0530

    folder bug fixed

--- main/main.py
+++ main/main.py
@@ -1,7 +1,7 @@
 '''
 File duplicate finder. Special mode for comparing images too.
 Created on 19-Feb-2021
-@author: navin
+@author: Navin
 '''
 
 # if running in py3, change the shebang, drop the next import for readability (it does no harm in py3)
@@ -147,17 +147,21 @@ class FileSearchBinaryMode:
             for fileOrdinal in range(len(filenames)):#for each file in the folder
                 filesize = self.fileSizes[folderOrdinal][fileOrdinal]
                 filename = self.filesInFolder[folderOrdinal][fileOrdinal]
+                if self.fileSizes[folderOrdinal][fileOrdinal] == self.alreadyProcessedFile:
+                    continue
                 #---compare with all files
-                for folderOrdinalToCompare in range(len(self.folderPaths)):#for each folder 
-                    for fileOrdinalToCompare in range(len(filenames)):#for each file in the folder
+                for folderOrdinalToCompare in range(len(self.folderPaths)):#for each folder
+                    filenamesToCompare = self.filesInFolder[folderOrdinalToCompare] 
+                    for fileOrdinalToCompare in range(len(filenamesToCompare)):#for each file in the folder
+                        filenameToCompare = self.filesInFolder[folderOrdinalToCompare][fileOrdinalToCompare]
                         if folderOrdinal == folderOrdinalToCompare and fileOrdinal == fileOrdinalToCompare:#skip self
                             continue
-                        if self.fileSizes[folderOrdinalToCompare][fileOrdinalToCompare] == self.alreadyProcessedFile:
+                        if filenameToCompare == self.alreadyProcessedFile:
                             continue
+                        
                         filesizeToCompare = self.fileSizes[folderOrdinalToCompare][fileOrdinalToCompare]
                         if filesize == filesizeToCompare:#initial match found based on size
-                            pathToCompare = self.folderPaths[folderOrdinalToCompare]
-                            filenameToCompare = self.filesInFolder[folderOrdinalToCompare][fileOrdinalToCompare]
+                            pathToCompare = self.folderPaths[folderOrdinalToCompare]                                                        
                             #---now compare based on file contents
                             filesAreSame = self.__compareEntireFiles__(path + filename, pathToCompare + filenameToCompare)
                             if filesAreSame:
@@ -166,7 +170,8 @@ class FileSearchBinaryMode:
                 self.__markAlreadyProcessedFile__(folderOrdinal, fileOrdinal)
     
     def __moveFileToSeparateFolder__(self, folderOrdinal, fileOrdinal, folderOrdinalToCompare, fileOrdinalToCompare):
-        #if move not possible, copy and mention move issue in report
+        #Note: Empty files will be identified as duplicates of other empty files. It's normal.  
+        #TODO: if move not possible, copy and mention any move issues in the generated report
         folder = self.folderPaths[folderOrdinal]
         file = self.filesInFolder[folderOrdinal][fileOrdinal]        
         dupFolder = self.folderPaths[folderOrdinalToCompare]
@@ -214,108 +219,11 @@ if __name__ == '__main__':
     #---proceed with image search menu
     if userChoice == FileSearchModes.choice_imagePixels:
         pass
-        
-
     
-    #---get folder name
-#     layout = [  [sg.Text('Filename')],
-#                 [sg.Input(), sg.FileBrowse()], 
-#                 [sg.OK(), sg.Cancel()]] 
-#     
-#     window = sg.Window('Get filename example', layout)
-#     
-#     event, values = window.read()
-#     window.close()
-
-
-
-
-
-# def chunk_reader(fobj, chunk_size=1024):
-#     """Generator that reads a file in chunks of bytes"""
-#     while True:
-#         chunk = fobj.read(chunk_size)
-#         if not chunk:
-#             return
-#         yield chunk
-# 
-# 
-# def get_hash(filename, first_chunk_only=False, hash=hashlib.sha1):
-#     hashobj = hash()
-#     file_object = open(filename, 'rb')
-# 
-#     if first_chunk_only:
-#         hashobj.update(file_object.read(1024))
-#     else:
-#         for chunk in chunk_reader(file_object):
-#             hashobj.update(chunk)
-#     hashed = hashobj.digest()
-# 
-#     file_object.close()
-#     return hashed
-# 
-# 
-# def check_for_duplicates(paths, hash=hashlib.sha1):
-#     hashes_by_size = defaultdict(list)  # dict of size_in_bytes: [full_path_to_file1, full_path_to_file2, ]
-#     hashes_on_1k = defaultdict(list)  # dict of (hash1k, size_in_bytes): [full_path_to_file1, full_path_to_file2, ]
-#     hashes_full = {}   # dict of full_file_hash: full_path_to_file_string
-# 
-#     for path in paths:
-#         for dirpath, dirnames, filenames in os.walk(path):
-#             # get all files that have the same size - they are the collision candidates
-#             for filename in filenames:
-#                 full_path = os.path.join(dirpath, filename)
-#                 try:
-#                     # if the target is a symlink (soft one), this will 
-#                     # dereference it - change the value to the actual target file
-#                     full_path = os.path.realpath(full_path)
-#                     file_size = os.path.getsize(full_path)
-#                     hashes_by_size[file_size].append(full_path)
-#                 except (OSError,):
-#                     # not accessible (permissions, etc) - pass on
-#                     continue
-# 
-# 
-#     # For all files with the same file size, get their hash on the 1st 1024 bytes only
-#     for size_in_bytes, files in hashes_by_size.items():
-#         if len(files) < 2:
-#             continue    # this file size is unique, no need to spend CPU cycles on it
-# 
-#         for filename in files:
-#             try:
-#                 small_hash = get_hash(filename, first_chunk_only=True)
-#                 # the key is the hash on the first 1024 bytes plus the size - to
-#                 # avoid collisions on equal hashes in the first part of the file
-#                 # credits to @Futal for the optimization
-#                 hashes_on_1k[(small_hash, size_in_bytes)].append(filename)
-#             except (OSError,):
-#                 # the file access might've changed till the exec point got here 
-#                 continue
-# 
-#     # For all files with the hash on the 1st 1024 bytes, get their hash on the full file - collisions will be duplicates
-#     for __, files_list in hashes_on_1k.items():
-#         if len(files_list) < 2:
-#             continue    # this hash of fist 1k file bytes is unique, no need to spend cpy cycles on it
-# 
-#         for filename in files_list:
-#             try: 
-#                 full_hash = get_hash(filename, first_chunk_only=False)
-#                 duplicate = hashes_full.get(full_hash)
-#                 if duplicate:
-#                     print("Duplicate found: {} and {}".format(filename, duplicate))
-#                 else:
-#                     hashes_full[full_hash] = filename
-#             except (OSError,):
-#                 # the file access might've changed till the exec point got here 
-#                 continue
-
-
-
-
-#     if sys.argv[1:]:
-#         check_for_duplicates(sys.argv[1:])
-#     else:
-#         print("Please pass the paths to check as parameters to the script")
+    #---specify what file to remove from folder and subfolders
+    if userChoice == FileSearchModes.choice_residualFiles:
+        pass
+        
 
 
 # import os, sys
